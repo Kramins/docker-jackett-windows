fbf5a9f792ed1f7353a9b936dca4776cee8b94d0
Chris Kramins <chris.kramins+github@gmail.com>
2018-06-07 16:21:55 -0400

    Updated to version: 0.8.1065

--- build.ps1
+++ build.ps1
@@ -1,13 +1,12 @@
 $ErrorActionPreference = "Stop"
 . .\build.config.ps1
 
-$JACKETT_VERSION = $env:DOCKER_APPLICATION_VERSION
 
 $imageFullName = ("{0}/{1}:{2}-windowsservercore" -f $env:DOCKER_REPO, $env:DOCKER_IMAGE, $env:DOCKER_APPLICATION_VERSION)
 $imageLatestName = ("{0}/{1}:latest" -f $env:DOCKER_REPO, $env:DOCKER_IMAGE)
 
 Write-Host `Building $imageFullName`
-docker build --build-arg JACKETT_VERSION=$JACKETT_VERSION . -t $imageFullName
+docker build --build-arg $env:DOCKER_BUILD_ARGS . -t $imageFullName
 
 Write-Host "Tagging $imageLatestName"
 docker tag $imageFullName  $imageLatestName
