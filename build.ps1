6b4520ff37aa56548fe22123bd1aec27a4204c9b
Chris Kramins <chris.kramins+github@gmail.com>
2018-06-07 14:53:24 -0400

    Build script normalization.

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 
 $JACKETT_VERSION = $env:DOCKER_APPLICATION_VERSION
 
-$imageFullName = ("{0}/{1}:{2}-windowsservercore" -f $env:DOCKER_REPO, $env:DOCKER_IMAGE, $UNIFI_VERSION)
+$imageFullName = ("{0}/{1}:{2}-windowsservercore" -f $env:DOCKER_REPO, $env:DOCKER_IMAGE, $env:DOCKER_APPLICATION_VERSION)
 $imageLatestName = ("{0}/{1}:latest" -f $env:DOCKER_REPO, $env:DOCKER_IMAGE)
 
 Write-Host `Building $imageFullName`
