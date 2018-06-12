11ae5634de8c89122d391c60ba6033a5184a6717
Chris Kramins <chris.kramins+github@gmail.com>
2018-06-12 08:40:05 -0400

    Fix test script

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 . .\build.config.ps1
 
-$imageFullName = ("{0}/{1}:{2}-windowsservercore" -f $env:DOCKER_REPO, $env:DOCKER_IMAGE, $UNIFI_VERSION)
-
+$imageFullName = ("{0}/{1}:{2}-windowsservercore" -f $env:DOCKER_REPO, $env:DOCKER_IMAGE, $env:DOCKER_APPLICATION_VERSION)
+Write-Host $imageFullName
 docker run -v d:/temp/config/jackett:c:/config -p 9117:9117 --rm -it $imageFullName
